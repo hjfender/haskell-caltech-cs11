422efe8b86618d2e7f1552bb51d7657acf769ce7
Henry Fender <henry@totient.local>
2018-05-30 04:33:05 -0500

    finished lab3

--- code/lab3part2.hs
+++ code/lab3part2.hs
@@ -2,7 +2,7 @@ module Lab3part2 (Quaternion
                  ) where
 
 {- Quaternion Type -}
-data Quaternion = Quaternion Double Double Double Double
+data Quaternion = Quaternion { r, i, j, k :: Double }
                 deriving (Eq)
 
 instance Show Quaternion where
@@ -17,3 +17,33 @@ another can equal a negative quaternion. However, i*i = k*k = j*j = -1, contradi
 
 Thus the quaternions cannot be usefully ordered for an Ord implementation -}
 
+instance Num Quaternion where
+(+) (Quaternion a1 b1 c1 d1) (Quaternion a2 b2 c2 d2) = Quaternion ((Prelude.+) a1 a2) ((Prelude.+) b1 b2) ((Prelude.+) c1 c2) ((Prelude.+) d1 d2)
+(-) (Quaternion a1 b1 c1 d1) (Quaternion a2 b2 c2 d2) = Quaternion ((Prelude.-) a1 a2) ((Prelude.-) b1 b2) ((Prelude.-) c1 c2) ((Prelude.-) d1 d2)
+(*) (Quaternion a1 b1 c1 d1) (Quaternion a2 b2 c2 d2) = Quaternion (foldl (Prelude.-) ((Prelude.*) a1 a2) [((Prelude.*) b1 b2),((Prelude.*) c1 c2),((Prelude.*) d1 d2)])
+                                                                   (foldl (Prelude.+) (negate((Prelude.*) d1 c2)) [((Prelude.*) c1 d2),((Prelude.*) a1 b2),((Prelude.*) b1 a2)])
+                                                                   (foldl (Prelude.+) (negate((Prelude.*) b1 d2)) [((Prelude.*) d1 b2),((Prelude.*) a1 c2),((Prelude.*) c1 a2)])
+                                                                   (foldl (Prelude.+) (negate((Prelude.*) c1 b2)) [((Prelude.*) b1 c2),((Prelude.*) a1 d2),((Prelude.*) d1 a2)])
+abs (Quaternion a b c d) = Quaternion (sqrt(foldr (Prelude.+) 0.0 [((Prelude.*) a a),((Prelude.*) b b),((Prelude.*) c c),((Prelude.*) d d)])) 0 0 0
+signum x@(Quaternion a b c d) = Quaternion (a / (r $ Lab3part2.abs x)) (b / (r $ Lab3part2.abs x)) (c / (r $ Lab3part2.abs x)) (d / (r $ Lab3part2.abs x))
+fromInteger i = Quaternion (i :: Double) 0 0 0
+
+testQuaternions :: IO ()
+testQuaternions = do s1 <- return ("i*i: " ++ Lab3part2.show ((Lab3part2.*) (Quaternion 0.0 1.0 0.0 0.0) (Quaternion 0.0 1.0 0.0 0.0)))
+                     putStrLn s1
+                     s2 <- return ("j*j: " ++ Lab3part2.show ((Lab3part2.*) (Quaternion 0.0 0.0 1.0 0.0) (Quaternion 0.0 0.0 1.0 0.0)))
+                     putStrLn s2
+                     s3 <- return ("k*k: " ++ Lab3part2.show ((Lab3part2.*) (Quaternion 0.0 0.0 0.0 1.0) (Quaternion 0.0 0.0 0.0 1.0)))
+                     putStrLn s3
+                     s4 <- return ("i*j: " ++ Lab3part2.show ((Lab3part2.*) (Quaternion 0.0 1.0 0.0 0.0) (Quaternion 0.0 0.0 1.0 0.0)))
+                     putStrLn s4
+                     s5 <- return ("j*i: " ++ Lab3part2.show ((Lab3part2.*) (Quaternion 0.0 0.0 1.0 0.0) (Quaternion 0.0 1.0 0.0 0.0)))
+                     putStrLn s5
+                     s6 <- return ("j*k: " ++ Lab3part2.show ((Lab3part2.*) (Quaternion 0.0 0.0 1.0 0.0) (Quaternion 0.0 0.0 0.0 1.0)))
+                     putStrLn s6
+                     s7 <- return ("k*j: " ++ Lab3part2.show ((Lab3part2.*) (Quaternion 0.0 0.0 0.0 1.0) (Quaternion 0.0 0.0 1.0 0.0)))
+                     putStrLn s7
+                     s8 <- return ("k*i: " ++ Lab3part2.show ((Lab3part2.*) (Quaternion 0.0 0.0 0.0 1.0) (Quaternion 0.0 1.0 0.0 0.0)))
+                     putStrLn s8
+                     s9 <- return ("i*k: " ++ Lab3part2.show ((Lab3part2.*) (Quaternion 0.0 1.0 0.0 0.0) (Quaternion 0.0 0.0 0.0 1.0)))
+                     putStrLn s9
